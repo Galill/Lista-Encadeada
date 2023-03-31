4c79ccf18713fd596dea98257c97443486933bef
Kalil Teotonio <80278976+Galill@users.noreply.github.com>
2023-03-30 21:45:16 -0300

    Update No.java

--- No.java
+++ No.java
@@ -9,18 +9,22 @@ public class No {
         this.proximo = null;
     }
 
+    /*Recebe o conteudo relacionado ao No*/
     public int getConteudo(){
         return conteudo;
     }
 
+    /*Confere o conteudo do No*/
     public void setConteudo(int conteudo){
         this.conteudo = conteudo;
     }
 
+    /*Aponta para o próximo No*/
     public No getProximo(){
         return proximo;
     }
 
+    /*Escolhe para quem o No vai apontar*/
     public void setProximo(No proximo){
         this.proximo = proximo;
     }
