08e8dbb1c48a7751c7ebadacd709be89a89c797b
Kalil Teotonio <80278976+Galill@users.noreply.github.com>
2023-03-30 21:45:37 -0300

    Update MainLinkedList.java

--- MainLinkedList.java
+++ MainLinkedList.java
@@ -9,11 +9,11 @@ public class MainLinkedList {
         int dado, pos;
         char x = 0, y = 0, w = 0, z = 0;
 
-        //Verifica se a lista criada é vazia
+        //Verifica se a lista criada é vazia.
         System.out.println("Retorna TRUE caso a lista esteja vazia e FALSE caso tenha algum elemento nela: " + list.vazia());
         System.out.println(" ");
 
-        //Pede ao usuário para inserir valores na lista 
+        //Pede ao usuário para inserir valores na lista. 
         while(x != 'n'){        
             System.out.println("Digite a posição do valor a ser inserido: ");
                 pos = sc.nextInt();     
@@ -24,18 +24,22 @@ public class MainLinkedList {
                 x = sc.next().charAt(0);
         }
         
+        //Printa a lista com os valores dados pelo usuário.
         System.out.println(" ");
         System.out.println("Lista gerada: ");
         for(int i = 1; i <= list.tamanho(); i++){
             System.out.println(list.elemento(i));
         }
-  
+        
         System.out.println(" ");
+        //Verifica se a lista continua vazia.
         System.out.println("Retorna TRUE caso a lista esteja vazia e FALSE caso tenha algum elemento nela: " + list.vazia());
         System.out.println(" ");
+        //Verifica o tamanho da lista.
         System.out.println("Quantidade de elementos da sua lista: " + list.tamanho());
         System.out.println(" ");
 
+        //Pede ao usuário para modificar um dos valores da lista.
         while(y != 'n'){    
             System.out.println("Digite a posição do valor a ser modificado: ");       
                 pos = sc.nextInt();
@@ -46,12 +50,14 @@ public class MainLinkedList {
                 y = sc.next().charAt(0);
         }
 
+        //Printa a lista com o valor modificado.
         System.out.println(" ");
         System.out.println("Nova lista gerada: ");
         for(int i = 1; i <= list.tamanho(); i++){
             System.out.println(list.elemento(i));
             }
 
+        //Pede ao usuário para verificar um dos valores da lista e retorna o valor na posição pedida.
         while(w != 'n'){    
             System.out.println("Digite a posição do valor a ser verificado: ");       
                 pos = sc.nextInt();
@@ -61,6 +67,7 @@ public class MainLinkedList {
                 w = sc.next().charAt(0);
         }
 
+        //Pede ao usuário para remover um dos valores da lista.
         while(z != 'n'){    
             System.out.println("Digite a posição do valor a ser removido: ");       
                 pos = sc.nextInt();
@@ -69,6 +76,7 @@ public class MainLinkedList {
                 z = sc.next().charAt(0);
         }
 
+        //printa a lista com o valor removido.
         System.out.println(" ");
         System.out.println("Nova lista gerada: ");
         for(int i = 1; i <= list.tamanho(); i++){
