4f2e72af414dd38022455a5690baac4105ae2237
Kalil Teotonio <80278976+Galill@users.noreply.github.com>
2023-03-30 21:46:08 -0300

    Update Lista.java

--- Lista.java
+++ Lista.java
@@ -2,17 +2,23 @@ package ListaEncadeada;
 
 public interface Lista {
     
+    /*Verifica se a lista está vazia.*/
     public boolean vazia();
 
-    public boolean cheia();
+    //public boolean cheia();  esse metodo não é necessário pois listas encadeadas não tem tamanho máximo.
 
+    /*Retorna o tamanho da lista.*/ 
     public int tamanho();
 
+    /*Retorna o elemento dada uma posição na lista.*/ 
     public int elemento(int pos);
 
+    /*Retorna a posição de um dado inserido.*/
     public int posicao(int dado);
 
+    /*Insere um dado na posição escolhida.*/
     public boolean insere(int pos, int dado);
 
+    /*Remove um dado da posição escolhida.*/
     public int remove(int pos);
 }
